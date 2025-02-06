edfd5ae24f2b6fe41427276073dd456b92b6b84c
Ville Halonen <71012693+hzville@users.noreply.github.com>
2025-02-06 04:49:48 +0200

    13.22

--- src/controllers/readingList.js
+++ src/controllers/readingList.js
@@ -1,5 +1,6 @@
 import express from "express";
-import { ReadingList } from "../models/index.js";
+import { ReadingList, User } from "../models/index.js";
+import tokenExtractor from "../utils/tokenExtractor.js";
 
 const readingListRouter = express.Router();
 
@@ -7,4 +8,17 @@ readingListRouter.post("/", async (req, res) => {
   const readingList = await ReadingList.create(req.body);
   return res.json(readingList);
 });
+
+readingListRouter.put("/:id", tokenExtractor, async (req, res) => {
+  const user = await User.findByPk(req.decodedToken.id);
+  const blog = await ReadingList.findByPk(req.params.id);
+  if (user.id === blog.userId) {
+    blog.read = req.body.read;
+    await blog.save();
+    res.json(blog);
+  } else {
+    res.status(404).end();
+  }
+});
+
 export default readingListRouter;
